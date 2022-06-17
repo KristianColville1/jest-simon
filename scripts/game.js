8f2030f4a73a306ff421802e2d9190e8e6ee4921
KristianColville1 <kristiancolville1@gmail.com>
2022-06-17 17:52:13 +0100

    Add tests for lights on and addturn

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,33 @@
-const { choices } = require("yargs");
-
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
-}
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
-function newGame(){
-    game.score = 0;
+function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
+    game.score = 0;
     showScore();
     addTurn();
 }
 
-function addTurn(){
+function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random() * choices.length))])
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-function lightsOn(circ){
-    document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light");
-    }, 400);
-}
-
-
-module.exports = {game, newGame, showScore, addTurn, lightsOn};
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -26,7 +26,7 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
-    test("choices contain the correct ids", () => {
+    test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
@@ -42,37 +42,36 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should be one element in computer sequence array", () => {
-        expect(game.currentGame.length).toBe(1);
-    });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
 });
 
-describe("game play works correctly", () => {
+describe("gameplay works correctly", () => {
     beforeEach(() => {
         game.score = 0;
         game.currentGame = [];
         game.playerMoves = [];
         addTurn();
     });
-    beforeAll(() => {
+    afterEach(() => {
         game.score = 0;
         game.currentGame = [];
         game.playerMoves = [];
     });
-    test('addTurn adds a new turn to the game', () => {
+    test("addTurn adds a new turn to the game", () => {
         addTurn();
         expect(game.currentGame.length).toBe(2);
     });
-    test('should add correct class to light up the buttons', () => {
-        let button  = document.getElementById(game.currentGame[0]);
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
-        expect(button.classList).toContain("light");
+        expect(button.classList).toContain(game.currentGame[0] + "light");
     });
 });
